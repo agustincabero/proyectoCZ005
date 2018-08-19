4c53e3f93e4b214bc2f995c5f296e0c591207b0e
Agustín Cabero <agustincabero@gmail.com>
2018-08-19 12:08:15 -0300

    Se elimina archivo intro.js ya que era un archivo de prueba

--- js/intro.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('load', function (Juego) {
-// // var newDiv = document.createElement('div');
-// // newDiv.innerHTML = '<img src="imagenes/mensaje1.png">'
-// // document.body.insertBefore(newDiv, document.body.firstElementChild);
-
-// setTimeout(Juego.iniciarRecursos, 3000);
-// })
